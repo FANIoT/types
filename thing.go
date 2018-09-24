e1578e4e91b2af23d3faff3ae9d53894e7297f90
Parham Alvani <parham.alvani@gmail.com>
2018-09-24 07:48:14 +0000

    Adds model and thing types.

---
+++ thing.go
@@ -0,0 +1,26 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 07-02-2018
+ * |
+ * | File Name:     thing/thing.go
+ * +===============================================
+ */
+
+package types
+
+// Thing contains identification and parent project of a thing
+// In this platform, a thing is every device that can connect to the platform directly using the link component provided interfaces.
+type Thing struct {
+	ID     string   `json:"id" bson:"_id,omitempty"` // thing unique identifier
+	Name   string   `json:"name" bson:"name"`        // thing human readable name
+	Status bool     `json:"status" bson:"status"`    // active/inactive
+	Model  string   `json:"model" bson:"model"`      // model describes how to decode an incoming payload
+	Tokens []string `json:"tokens" bson:"tokens"`    // thing access tokens that are generated based on K-Sortable Globally Unique IDs
+
+	Assets         map[string]Asset       `json:"assets" bson:"assets"`                 // thing assets (sensors and actuators)
+	Connectivities map[string]interface{} `json:"connectivities" bson:"connectivities"` // supported connectivities (like TheThingsNetwork and etc.)
+
+	Project string `json:"project" bson:"project"`
+}
