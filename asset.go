c37dcb752bfa30925e675e9348e1d73f408e572c
Parham Alvani <parham.alvani@gmail.com>
2018-12-03 11:10:24 +0330

    Adds location type.

--- asset.go
+++ asset.go
@@ -14,8 +14,11 @@
 package types
 
 // Asset is sensor or actuator that is attached to a thing.
+// Asset data type is only applies when data is retried from database.
+// Database only recognizes following types:
+// boolean, number, string, object and array
 type Asset struct {
 	Title string `json:"title" bson:"title"`
-	Type  string `json:"type" bson:"type"` // boolean, number, string, object and array
+	Type  string `json:"type" bson:"type"` // boolean, number, string, object, location and array
 	Kind  string `json:"kind" bson:"kind"` // sensor or actuator
 }
